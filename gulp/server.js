63a13d2bf3ca76350caab608739ba69e3c006a6c
lukasz.barc <barclukasz@gmail.com>
2016-08-11 22:58:21 +0200

    initial commit

---
+++ gulp/server.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+
+var nodemon = require('gulp-nodemon');
+
+
+gulp.task('dev:server', function() {
+    nodemon({
+        script: 'server.js',
+        ext: 'js',
+        ignore: ['ng*', 'gulp*', 'assets*']
+    });
+})
